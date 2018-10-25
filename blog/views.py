1eb68f425b175c178dd2af6bc51bba8e6be06527
samapraku <samapco@gmail.com>
2018-10-25 13:34:48 +0200

    Admin and Models

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'SamyAdu',
-        'title':'Blog Post 1',
-        'content':'First Post content',
-        'date_posted':'August 28, 2018'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'September 28, 2018'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
